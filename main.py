15293a07e7b1e98312c7081e654444693433fece
Tianda Huang <37414124+tiandahuang@users.noreply.github.com>
2022-10-05 15:07:42 -0500

    data recieve working

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import asyncio
 import sys
 from argparse import ArgumentParser
-from unittest.mock import NonCallableMagicMock
+from collections import OrderedDict
 
 from bleak import BleakScanner
 from bleak import BleakClient
+
+def hydration_print_data(byte_array):
+    unsigned = lambda b: int.from_bytes(b, byteorder='little', signed=False)
+    signed = lambda b: int.from_bytes(b, byteorder='little', signed=True)
+
+    DATA_SPLIT = OrderedDict({
+                    'num':{'len':2, 'convert':unsigned},
+                    'resistance':{'len':4, 'convert':signed},
+                    'reactance':{'len':4, 'convert':signed},
+                    'temp 1':{'len':2, 'convert':unsigned},
+                    'temp 2':{'len':2, 'convert':unsigned}
+                })
+    EXPECTED_LEN = sum(map(lambda x: x['len'], DATA_SPLIT.values()))
+
+    if len(byte_array) != EXPECTED_LEN:
+        print(f'error {len(byte_array)} bytes recieved, {EXPECTED_LEN} bytes expected')
+        print(byte_array)
+        return
+    idx = 0
+    for key in DATA_SPLIT:
+        data = DATA_SPLIT[key]['convert'](byte_array[idx:idx+DATA_SPLIT[key]['len']])
+        print(f'{key}: {data}', end=' | ')
+        idx += DATA_SPLIT[key]['len']
+    print('')
 
 async def find_device(name):
     stop_event = asyncio.Event()
@@ -26,32 +50,50 @@ async def find_device(name):
     print(advertising_data)
     return device, advertising_data
 
-async def main():
 
-    parser = ArgumentParser(description='Connect to a BLE device')
-    parser.add_argument('name')
-    parser.add_argument('--timeout', '-t', dest='timeout', default=5)
-    parser.add_argument('--gatt_char', '-g', dest='gatt_descriptor', default='Nordic UART TX')
-    args = parser.parse_args()
+async def device_connect(device, args):
 
-    device, _ =  await find_device(args.name)
+    disconnect_event = asyncio.Event()
+
+    def disconnect_callback(dev):
+        disconnect_event.set()
+        print(f'disconnected: {dev}')
+
+    def recieve_callback(gatt_char, data):
+        hydration_print_data(data)
+
     try:
-        async with BleakClient(device) as client:
+        async with BleakClient(device, disconnect_callback, timeout=args.timeout) as client:
             characteristics = client.services.characteristics.copy()
 
             if any((gatt_nordic_uart_tx := gatt).description == args.gatt_descriptor 
                    for gatt in characteristics.values()):
                 print(gatt_nordic_uart_tx)
-            print(await client.read_gatt_char(gatt_nordic_uart_tx))
-            print(client)
+
+            await client.start_notify(gatt_nordic_uart_tx, recieve_callback)
+            await disconnect_event.wait()
+
     except asyncio.exceptions.CancelledError:
-        print('cancelled')
+        print('connection cancelled')
     except asyncio.exceptions.TimeoutError:
-        print('timeout')
+        print('connection timeout')
+
+
+async def main():
+
+    parser = ArgumentParser(description='Connect to a BLE device')
+    parser.add_argument('name')
+    parser.add_argument('--timeout', '-t', dest='timeout', default=20.0)
+    parser.add_argument('--gatt_char', '-g', dest='gatt_descriptor', default='Nordic UART TX')
+    args = parser.parse_args()
+
+    device, _ =  await find_device(args.name)
+    await device_connect(device, args)
 
     # TODO: is 'Nordic UART TX' always the last GATT handle
     # TODO: handle for incoming messages
     # TODO: longer timeout, handle timeout
+    # TODO: gather for multiple devices
 
 
 if __name__ == '__main__':
