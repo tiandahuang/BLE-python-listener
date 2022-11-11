fcb350cb6b36095c8ec0f799865bda2bc88edf91
Tianda Huang <37414124+tiandahuang@users.noreply.github.com>
2022-11-11 14:00:15 -0600

    better graphing

--- dataprocessing/dataprocessing.py
+++ dataprocessing/dataprocessing.py
@@ -1,12 +1,12 @@
 import multiprocessing as mp
-import time
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 from collections import deque
 from collections import OrderedDict
 
-PLOT_REFRESH_RATE = 100
+PLOT_REFRESH_RATE = 200
 
 QUEUE_BASE_LEN = 100
 unsigned = lambda b: int.from_bytes(b, byteorder='little', signed=False)
@@ -19,15 +19,15 @@ signed = lambda b: int.from_bytes(b, byteorder='little', signed=True)
 #                 'temp 2':{'len':2, 'convert':unsigned}
 #             })
 DATA_SPLIT = OrderedDict({
-                'num':{'len':2, 'convert':unsigned},
-                'scg':{'len':3, 'convert':signed, 'multi':20},
-                'ecg':{'len':3, 'convert':signed, 'multi':20},
-                'ppg':{'len':4, 'convert':signed, 'multi':5},
-                'tmp':{'len':2, 'convert':unsigned}
+                'num':{'len':2, 'convert':unsigned, 'color':(80/256,0,0)},
+                'scg':{'len':3, 'convert':signed, 'multi':20, 'color':(80/256,0,0)},
+                'ecg':{'len':3, 'convert':signed, 'multi':20, 'color':(80/256,0,0)},
+                'ppg':{'len':4, 'convert':signed, 'multi':5, 'color':(80/256,0,0)},
+                'tmp':{'len':2, 'convert':unsigned, 'color':(80/256,0,0)}
             })
 EXPECTED_LEN = sum(map(lambda x: x['len']*x.get('multi', 1), DATA_SPLIT.values()))
 maxlen = lambda key : QUEUE_BASE_LEN*DATA_SPLIT[key].get('multi', 1)
-DATA_RANGES = {key:np.linspace(0, 1, maxlen(key)) for key in DATA_SPLIT}
+# DATA_RANGES = {key:np.linspace(0, 1, maxlen(key)) for key in DATA_SPLIT}
 
 
 
@@ -70,9 +70,6 @@ class DataStream(mp.Process):
                 self.graphics_queues[key].append(data)
                 # print(f'{key}:{data}', end='|')
         # print('')
-    # def parse_data(self) -> None:
-    #     data = self.q.get(block=True)
-    #     print(data)
 
     def run(self) -> None:
         while True:
@@ -94,14 +91,24 @@ class DataPlotting:
 
     def __init__(self, graphics_queues : dict[deque], *args, **kwargs) -> None:
         self.queues = graphics_queues
+
+        SUBPLOT_ROWS = len(graphics_queues)
+        SUBPLOT_COLS = 1
+
         self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(1,1,1)
+        self.axes = {key:self.fig.add_subplot(SUBPLOT_ROWS, SUBPLOT_COLS, i+1) 
+                     for i, key in enumerate(graphics_queues)}
+        # plot title, label, and color config
+        for key in self.axes:
+            self.axes[key].set_ylabel(key)
+            self.axes[key].xaxis.set_visible(False)
+            self.axes[key].plot(self.queues[key], color=DATA_SPLIT[key]['color'])
         plt.ion()
 
     def update(self):
-        self.fig.gca().cla()
-        for key in DATA_SPLIT:
-            self.ax.plot(DATA_RANGES[key], self.queues[key])
+        for key in self.axes:
+            self.axes[key].lines.pop()
+            self.axes[key].plot(self.queues[key], color=DATA_SPLIT[key]['color'])
         plt.draw()
         plt.pause(1/PLOT_REFRESH_RATE)
         pass
