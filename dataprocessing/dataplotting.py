19328cfd5d1c31c552ec9fb41a1ce73434f067b1
Tianda Huang <37414124+tiandahuang@users.noreply.github.com>
2023-03-22 17:36:33 -0500

    usage

--- dataprocessing/dataplotting.py
+++ dataprocessing/dataplotting.py
@@ -1,9 +1,13 @@
 import sys
 
 import matplotlib.pyplot as plt
+import numpy as np
+from typing import Union
+
+# for benchmark
 import time
 import math
-import numpy as np
+
 
 if __name__ == '__main__':
     from plottingutils import CircularBuffer, PlottingColor
@@ -14,17 +18,30 @@ class DataPlotting:
     """
     Plots streamed data from a single device using Matplotlib
     Uses blitting for responsive graph updates
+
+    For 1d live-plotting use CircularBuffer
+    For 2d live-plotting use np.ndarray
     """
 
-    def __init__(self, data_buffers : list, colors : list = None, *args, **kwargs) -> None:
+    def __init__(self, 
+                 data_buffers : Union[list[CircularBuffer], list[np.ndarray]], # required
+                 labels       : list[str] = None,
+                 colors       : list[Union[str, tuple[int, int, int], tuple[float, float, float]]] = None,
+                 ylims        : list[tuple[float, float]] = None,
+                 cols         : int = None,
+                 *args, **kwargs) -> None:
+
         self.data_2d = type(data_buffers[0]) is not CircularBuffer
         self.num_fields = len(data_buffers)
         self.buffers = data_buffers
+
         self.colors = list(map(PlottingColor.auto_normalize, colors)) if colors is not None else (
                       list(PlottingColor(self.num_fields)))
-        self.labels = ['' for i in range(self.num_fields)]      # TODO:
-        self.ylims = [(-1, 1) for i in range(self.num_fields)]  # TODO:
-        self.cols = 1                                           # TODO:
+        self.labels = self._check_arg(labels, 
+                                      ['' for i in range(self.num_fields)])
+        self.ylims = self._check_arg(ylims,
+                                     [(-1, 1) for i in range(self.num_fields)])
+        self.cols = self._check_arg(cols, 1, scalar=True)
 
         SUBPLOT_ROWS = -(self.num_fields // -self.cols)     # ceiling integer divide
         SUBPLOT_COLS = self.cols
@@ -40,16 +57,16 @@ class DataPlotting:
         self.fig.show()
         self.redraw()
 
-    def update(self):
+    def update(self) -> None:
         self._update_2d() if self.data_2d else self._update_1d()
 
-    def redraw(self, event=None):
+    def redraw(self, event=None) -> None:
         for i in range(self.num_fields): self.axes[i].cla()
         self._reset_axes()
         self._update_background()
         self.update()
 
-    def stop(self):
+    def stop(self) -> None:
         print('displaying last output. close figure to continue')
         self.fig.show()
 
@@ -92,6 +109,14 @@ class DataPlotting:
     def _update_background(self):
         self.backgrounds = [self.fig.canvas.copy_from_bbox(self.axes[i].bbox) 
                             for i in range(self.num_fields)]
+
+    def _check_arg(self, arg, default, scalar=False):
+        if arg is not None:
+            if not scalar:
+                if len(arg) != self.num_fields: raise ValueError
+            return arg
+        else:
+            return default
 
 if __name__ == '__main__':
     # test/benchmark
